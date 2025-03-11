bd237c48e9b5566c6fdab1866305a045d3412b58
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-03-11 22:56:58 +0700

    5. khoá thanh cuộn khi mở modal, tối ưu modal tránh giao diện bị giật

--- main.js
+++ main.js
@@ -2,7 +2,25 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 function Modal() {
-    this.openModal = function (content) {
+    const getScrollbarWidth = () => {
+        // Kiểm tra trước đó đã tính toán rồi thì trả về giá trị đã tính
+        if (getScrollbarWidth.value) return getScrollbarWidth.value;
+
+        const div = document.createElement("div");
+        Object.assign(div.style, {
+            position: "fixed",
+            top: "-999px",
+            overflow: "scroll",
+        });
+
+        document.body.appendChild(div);
+        const width = div.offsetWidth - div.clientWidth;
+        getScrollbarWidth.value = width;
+        document.body.removeChild(div);
+        return width;
+    };
+
+    this.openModal = function ({ templateId, allowClose = true }) {
         // Create element
         const backdrop = document.createElement("div");
         backdrop.className = "modal-backdrop";
@@ -18,7 +36,12 @@ function Modal() {
         modalContent.className = "modal-content";
 
         // Append content and elements
-        modalContent.innerHTML = content;
+        const template = $(`#${templateId}`);
+        if (template) {
+            const templateCloned = template.content.cloneNode(true);
+            modalContent.appendChild(templateCloned);
+        }
+
         container.append(closeBtn, modalContent);
         backdrop.append(container);
         document.body.append(backdrop);
@@ -27,17 +50,22 @@ function Modal() {
             backdrop.classList.add("show");
         }, 0);
 
+        // Thực hiện khoá cuộn của body
+        document.body.style.overflow = "hidden";
+        document.body.style.paddingRight = getScrollbarWidth() + "px";
+
         // Attach events
         closeBtn.onclick = () => {
             this.closeModal(backdrop);
         };
 
         backdrop.onclick = (e) => {
-            if (e.target === backdrop) {
+            if (e.target === backdrop && allowClose) {
                 this.closeModal(backdrop);
             }
         };
 
+        // Xử lý sự kiện khi nhấn phím ESC -> Đóng modal
         const onKeyDown = (e) => {
             if (e.key === "Escape") {
                 this.closeModal(backdrop);
@@ -46,6 +74,8 @@ function Modal() {
         };
 
         document.addEventListener("keydown", onKeyDown);
+
+        return backdrop;
     };
 
     this.closeModal = function (modalElement) {
@@ -53,17 +83,30 @@ function Modal() {
         modalElement.ontransitionend = function () {
             modalElement.remove();
         };
+        // Thực hiện mở cuộn của body
+        document.body.style.overflow = "auto";
+        document.body.style.paddingRight = "";
     };
 }
 
 const modal = new Modal();
 
-// modal.openModal("<h1>Hello F8</h1>");
-
 $("#modal-btn-1").onclick = function () {
-    modal.openModal("<h1>Hello guys</h1>");
-};
+    const modalContent = modal.openModal({
+        templateId: "template-login",
+        allowClose: false,
+    });
 
-$("#modal-btn-2").onclick = function () {
-    modal.openModal("<h1>Hello guys 2</h1>");
+    const loginForm = modalContent.querySelector("#form-login");
+    loginForm.onsubmit = function (e) {
+        e.preventDefault();
+        const values = {
+            email: loginForm.email.value,
+            password: loginForm.password.value,
+        };
+
+        console.log({ values });
+
+        modal.closeModal(modalContent);
+    };
 };
