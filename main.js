2cb828e7f31421300ad4e25e5a834504d069a932
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-03-20 17:40:31 +0700

    7. chuyển phương thức thành prototype

--- main.js
+++ main.js
@@ -3,6 +3,188 @@ const $$ = document.querySelectorAll.bind(document);
 
 Modal.elements = [];
 
+// Xử lý tối ưu dùng chung cho tất cả modal
+Modal.prototype._createButton = function (text, className, onClick) {
+    const button = document.createElement("button");
+    button.className = className;
+    button.innerHTML = text;
+    button.onclick = onClick;
+    return button;
+};
+
+// Xử lý tối ưu dùng chung cho tất cả modal
+Modal.prototype._getScrollbarWidth = function () {
+    // Kiểm tra trước đó đã tính toán rồi thì trả về giá trị đã tính
+    if (this._scrollbarWidth) return this._scrollbarWidth;
+    const div = document.createElement("div");
+    Object.assign(div.style, {
+        position: "fixed",
+        top: "-999px",
+        overflow: "scroll",
+    });
+
+    document.body.appendChild(div);
+    const width = div.offsetWidth - div.clientWidth;
+    this._scrollbarWidth = width;
+    document.body.removeChild(div);
+    return width;
+};
+
+Modal.prototype._buildModal = function () {
+    // Create backdrop
+    this._backdrop = document.createElement("div");
+    this._backdrop.className = "modal-backdrop";
+
+    // Create container
+    const container = document.createElement("div");
+    container.className = "modal-container";
+
+    // Create modal content
+    const modalContent = document.createElement("div");
+    modalContent.className = "modal-content";
+
+    // Append content and elements
+    const templateCloned = this._template.content.cloneNode(true);
+    modalContent.append(templateCloned);
+
+    // Thêm close button vào modal content
+    if (this._allowButtonClose) {
+        const closeBtn = this._createButton("&times;", "modal-close", this.close.bind(this));
+        modalContent.append(closeBtn);
+    }
+
+    container.append(modalContent);
+
+    // Chèn footer vào modal
+    if (this._options.footer) {
+        this._footerModal = document.createElement("div");
+        this._footerModal.className = "modal-footer";
+        this._renderFooterContent();
+        this._renderFooterButtons();
+        container.append(this._footerModal);
+    }
+
+    // Chèn header vào modal
+    if (this._options.header) {
+        const header = document.createElement("div");
+        header.className = "modal-header";
+        header.innerHTML = "Header content";
+        container.prepend(header);
+    }
+
+    container.classList.add(...this._options.cssClassContainer);
+    this._backdrop.classList.add(...this._options.cssClassBackdrop);
+    this._backdrop.append(container);
+    document.body.append(this._backdrop);
+};
+
+Modal.prototype._ontransitionend = function (callback) {
+    this._backdrop.ontransitionend = (event) => {
+        if (event.propertyName !== "opacity") return;
+
+        if (typeof callback === "function") callback();
+    };
+};
+
+Modal.prototype._handleEscapeKey = function (e) {
+    console.log(this);
+    if (this !== Modal.elements[Modal.elements.length - 1]) return;
+
+    if (e.key === "Escape") {
+        this.close();
+    }
+};
+
+Modal.prototype.open = function () {
+    if (!this._backdrop) {
+        this._buildModal();
+        Modal.elements.push(this);
+    }
+
+    setTimeout(() => {
+        this._backdrop.classList.add("show");
+    }, 0);
+
+    // Thực hiện khoá cuộn của body
+    document.body.style.overflow = "hidden";
+    document.body.style.paddingRight = this._getScrollbarWidth() + "px";
+
+    // Attach events
+    if (this._allowEscapeClose) {
+        document.addEventListener("keydown", this._handleEscapeKey);
+    }
+
+    if (this._allowBackdropClose) {
+        // Xử lý sự kiện khi nhấn vào backdrop -> Đóng modal
+        this._backdrop.onclick = (e) => {
+            if (e.target === this._backdrop) {
+                this.close();
+            }
+        };
+    }
+
+    this._ontransitionend(this._options.onOpen);
+
+    return this._backdrop;
+};
+
+Modal.prototype.close = function (destroy = this._options.destroyOnClose) {
+    console.log(destroy);
+    if (!this._backdrop) return; // Nếu trong trường hợp không có backdrop thì thôi không làm gì tránh lỗi
+
+    Modal.elements.pop();
+    this._backdrop.classList.remove("show");
+    document.removeEventListener("keydown", this._handleEscapeKey);
+
+    this._ontransitionend(() => {
+        if (destroy && this._backdrop) {
+            this._backdrop.remove();
+            this._backdrop = null;
+            this._footerModal = null;
+        }
+        // Thực hiện mở cuộn của body
+        if (!Modal.elements.length) {
+            document.body.style.overflow = "";
+            document.body.style.paddingRight = "";
+        }
+        if (typeof this._options.onClose === "function") this._options.onClose();
+    });
+};
+
+Modal.prototype.destroy = function () {
+    if (this._backdrop) {
+        this._backdrop.remove();
+        this._backdrop = null;
+    }
+};
+
+Modal.prototype.setFooterContent = function (content) {
+    if (!this._options.footer) return;
+    this._footerContent = content;
+    this._renderFooterContent();
+};
+
+Modal.prototype._renderFooterContent = function () {
+    if (this._footerModal && this._footerContent) {
+        this._footerModal.innerHTML = this._footerContent;
+    }
+};
+
+Modal.prototype._renderFooterButtons = function () {
+    this._footerActions.forEach((action) => {
+        if (this._footerModal) {
+            this._footerModal.append(action);
+        }
+    });
+};
+
+Modal.prototype.addFooterButton = function (title, className, onClick) {
+    if (!this._options.footer) return;
+    const button = this._createButton(title, className, onClick);
+    this._footerActions.push(button);
+    this._renderFooterButtons();
+};
+
 function Modal({
     templateId,
     cssClassContainer = [],
@@ -14,204 +196,28 @@ function Modal({
     header = false,
     destroyOnClose = true,
 }) {
-    const template = $(`#${templateId}`);
-    if (!template) {
-        console.error(`Template with id ${templateId} not found`);
+    this._options = {
+        templateId,
+        cssClassContainer,
+        cssClassBackdrop,
+        closeMethods,
+        onOpen,
+        onClose,
+        footer,
+        header,
+        destroyOnClose,
+    };
+
+    this._template = $(`#${this._options.templateId}`);
+    if (!this._template) {
+        console.error(`Template with id ${this._options.templateId} not found`);
         return;
     }
-
     this._allowBackdropClose = closeMethods.includes("backdrop");
     this._allowButtonClose = closeMethods.includes("button");
     this._allowEscapeClose = closeMethods.includes("esc");
-    this._destroyOnClose = destroyOnClose;
-    this._backdrop = null;
-    this._footerContent = null;
-    this._footerModal = null;
     this._footerActions = [];
-    this._scrollbarWidth = 0;
-
-    this._getScrollbarWidth = () => {
-        // Kiểm tra trước đó đã tính toán rồi thì trả về giá trị đã tính
-        if (this._scrollbarWidth) return this._scrollbarWidth;
-
-        const div = document.createElement("div");
-        Object.assign(div.style, {
-            position: "fixed",
-            top: "-999px",
-            overflow: "scroll",
-        });
-
-        document.body.appendChild(div);
-        const width = div.offsetWidth - div.clientWidth;
-        this._scrollbarWidth = width;
-        document.body.removeChild(div);
-        return width;
-    };
-
-    // Viết hàm tạo button để tái sử dụng
-    this._createButton = (text, className, onClick) => {
-        const button = document.createElement("button");
-        button.className = className;
-        button.innerHTML = text;
-        button.onclick = onClick;
-        return button;
-    };
-
-    this._buildModal = () => {
-        // Create backdrop
-        this._backdrop = document.createElement("div");
-        this._backdrop.className = "modal-backdrop";
-
-        // Create container
-        const container = document.createElement("div");
-        container.className = "modal-container";
-
-        // Create modal content
-        const modalContent = document.createElement("div");
-        modalContent.className = "modal-content";
-
-        // Append content and elements
-        const templateCloned = template.content.cloneNode(true);
-        modalContent.append(templateCloned);
-
-        // Thêm close button vào modal content
-        if (this._allowButtonClose) {
-            const closeBtn = this._createButton("&times;", "modal-close", () => this.close());
-            modalContent.append(closeBtn);
-        }
-
-        container.append(modalContent);
-
-        // Chèn footer vào modal
-        if (footer) {
-            this._footerModal = document.createElement("div");
-            this._footerModal.className = "modal-footer";
-            this._renderFooterContent();
-            this._renderFooterButtons();
-            container.append(this._footerModal);
-        }
-
-        // Chèn header vào modal
-        if (header) {
-            const header = document.createElement("div");
-            header.className = "modal-header";
-            header.innerHTML = "Header content";
-            container.prepend(header);
-        }
-
-        container.classList.add(...cssClassContainer);
-        this._backdrop.classList.add(...cssClassBackdrop);
-        this._backdrop.append(container);
-        document.body.append(this._backdrop);
-    };
-
-    this._ontransitionend = (callback) => {
-        this._backdrop.ontransitionend = (event) => {
-            if (event.propertyName !== "opacity") return;
-
-            if (typeof callback === "function") callback();
-        };
-    };
-
-    this._handleEscapeKey = (e) => {
-        if (this !== Modal.elements[Modal.elements.length - 1]) return;
-
-        if (e.key === "Escape") {
-            this.close();
-        }
-    };
-
-    // Method dùng để mở modal
-    this.open = function () {
-        if (!this._backdrop) {
-            this._buildModal();
-        }
-
-        Modal.elements.push(this);
-
-        setTimeout(() => {
-            this._backdrop.classList.add("show");
-        }, 0);
-
-        // Thực hiện khoá cuộn của body
-        document.body.style.overflow = "hidden";
-        document.body.style.paddingRight = this._getScrollbarWidth() + "px";
-
-        // Attach events
-        if (this._allowEscapeClose) {
-            document.addEventListener("keydown", this._handleEscapeKey);
-        }
-
-        if (this._allowBackdropClose) {
-            // Xử lý sự kiện khi nhấn vào backdrop -> Đóng modal
-            this._backdrop.onclick = (e) => {
-                if (e.target === this._backdrop) {
-                    this.close();
-                }
-            };
-        }
-
-        this._ontransitionend(onOpen);
-
-        return this._backdrop;
-    };
-
-    // Method dùng để đóng modal
-    this.close = function (destroy = destroyOnClose) {
-        if (!this._backdrop) return; // Nếu trong trường hợp không có backdrop thì thôi không làm gì tránh lỗi
-
-        Modal.elements.pop();
-        this._backdrop.classList.remove("show");
-        document.removeEventListener("keydown", this._handleEscapeKey);
-
-        this._ontransitionend(() => {
-            if (destroy && this._backdrop) {
-                this._backdrop.remove();
-                this._backdrop = null;
-                this._footerModal = null;
-            }
-            // Thực hiện mở cuộn của body
-            if (!Modal.elements.length) {
-                document.body.style.overflow = "";
-                document.body.style.paddingRight = "";
-            }
-            if (typeof onClose === "function") onClose();
-        });
-    };
-
-    this.destroy = function () {
-        if (this._backdrop) {
-            this._backdrop.remove();
-            this._backdrop = null;
-        }
-    };
-
-    this.setFooterContent = (content) => {
-        if (!footer) return;
-        this._footerContent = content;
-        this._renderFooterContent();
-    };
-
-    this._renderFooterContent = () => {
-        if (this._footerModal && this._footerContent) {
-            this._footerModal.innerHTML = this._footerContent;
-        }
-    };
-
-    this._renderFooterButtons = () => {
-        this._footerActions.forEach((action) => {
-            if (this._footerModal) {
-                this._footerModal.append(action);
-            }
-        });
-    };
-
-    this.addFooterButton = (name, className, onClick) => {
-        if (!footer) return;
-        const button = this._createButton(name, className, onClick);
-        this._footerActions.push(button);
-        this._renderFooterButtons();
-    };
+    this._handleEscapeKey = this._handleEscapeKey.bind(this);
 }
 
 /**
@@ -248,7 +254,7 @@ const modal2 = new Modal({
 });
 
 modal.addFooterButton("OK", "btn btn-primary", function () {
-    modal2.open();
+    modal.open();
 });
 
 modal.addFooterButton("<span>Huỷ bỏ</span>", "btn btn-danger", function () {
